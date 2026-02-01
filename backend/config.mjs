09b001c45331545299e6172939cfb87640167eb4
Pp792863384 <cb0468186@gmail.com>
2026-02-01 15:39:36 +0530

    n

--- backend/config.mjs
+++ backend/config.mjs
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-let MONGO_URI= process.env.MONGO_URI;
+let mongodbURI= process.env.MONGO_URI;
 let port = process.env.port;
 let secret_token = process.env.secret_token;
-export {MONGO_URI, port, secret_token};
+export {mongodbURI, port, secret_token};
